a286669dc45e1cdb5f51b2326bac00cb04009a32
aimogue <ahmed.ibrahimmogue@gmail.com>
2020-12-30 09:30:51 -0500

    💄 Update UI: Added section articles to landing page

--- JS/app.js
+++ JS/app.js
@@ -20,13 +20,12 @@ const navigationSlide = () => {
   });
 };
 
-window.addEventListener("scroll", () => {
-  const headerBar = document.querySelector("header");
-  headerBar.classList.toggle("sticky-header", window.scrollY > 0);
-});
-
 const app = () => {
   navigationSlide();
+  window.addEventListener("scroll", () => {
+    const headerBar = document.querySelector("header");
+    headerBar.classList.toggle("sticky-header", window.scrollY > 0);
+  });
 };
 
 app();
